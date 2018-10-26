9fbaf67f72caace28a21ebd210f7529ccc9b2895
yura <ogay_yura@mail.ru>
2018-10-26 22:08:45 +0300

    Выполнено задание 6

--- lesson_01/lesson01_hw06.py
+++ lesson_01/lesson01_hw06.py
@@ -1,1 +1,14 @@
 # 6. Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+# ссылка на блок-схему
+# https://drive.google.com/file/d/1r1Yjl658QKjyqqjxZIETBo4Ie885wPGz/view?usp=sharing
+
+number = int(input('Введите номер буквы в алфавите '))
+
+symbols = 'abcdefghijklmnopqrstuvwxyz'
+
+if number > len(symbols):
+    print('Столько букв в алфавите нет')
+else:
+    symbol = symbols[number - 1]
+    print(symbol)
