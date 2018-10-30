25e784eb33f93ca8222e7c695b2032697de009da
yura <ogay_yura@mail.ru>
2018-10-30 22:26:31 +0300

    Выполнено задание 2

--- lesson_02/lesson02_hw02.py
+++ lesson_02/lesson02_hw02.py
@@ -1,3 +1,20 @@
 # 2. Посчитать четные и нечетные цифры введенного натурального числа.
 # Например, если введено число 34560, то у него 3 четные цифры (4, 6 и 0)
 # и 2 нечетные (3 и 5).
+
+# сылка на блок-схему
+# https://drive.google.com/file/d/1Fmr2zThT7H9T3t1F-uTJ79EULK2JbuTG/view?usp=sharing
+
+number = int(input('Введите натуральное число: '))
+even = 0
+odd = 0
+
+while number:
+    dig = number % 10
+    number = number // 10
+    if dig % 2:
+        odd += 1
+    else:
+        even += 1
+
+print('В числе цифры: {} - четные, {} - нечетные'.format(even, odd))
