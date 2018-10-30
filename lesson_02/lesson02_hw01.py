8a390f7c3f02cb3580114048d2ac1fbfe5ee8da8
yura <ogay_yura@mail.ru>
2018-10-30 21:47:18 +0300

    Выполнено задание 1

--- lesson_02/lesson02_hw01.py
+++ lesson_02/lesson02_hw01.py
@@ -8,3 +8,37 @@
 # ему об ошибке и снова запрашивать знак операции. Также сообщать
 # пользователю о невозможности деления на ноль, если он ввел 0
 # в качестве делителя.
+
+# ссылка на блок-схему
+# https://drive.google.com/file/d/1d7d-aJB1XpzeeiXnq1klXiypvc1ErAYk/view?usp=sharing
+
+while True:
+    print('Введите математическую операцию ("+", "-", "*", "/")')
+    print('или 0 - для завершения программы')
+    act = input('# ')
+
+    if act == '0':
+        print('Завершение программы!')
+        break
+
+    if act == '+' or act == '-' or act == '*' or act == '/':
+        dig1 = float(input('Введите первое число: '))
+        dig2 = float(input('Введите второе число: '))
+
+        if act == '/' and dig2 == 0:
+            print('Деление на ноль не возможно!')
+        else:
+            result = 0
+            if act == '+':
+                result = dig1 + dig2
+            elif act == '-':
+                result = dig1 - dig2
+            elif act == '*':
+                result = dig1 * dig2
+            else:
+                result = dig1 / dig2
+            print('Результат вычисления: ', result)
+    else:
+        print('Введена недопустимая операция!')
+
+    input('Нажмите Enter для продолжения')
