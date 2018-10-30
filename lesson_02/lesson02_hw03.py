01795eedc7feebf2608fe6eb1e7432f00c9939be
yura <ogay_yura@mail.ru>
2018-10-30 22:54:52 +0300

    Выполнено задание 3

--- lesson_02/lesson02_hw03.py
+++ lesson_02/lesson02_hw03.py
@@ -2,3 +2,15 @@
 # входящих в него цифр и вывести на экран. Например, если
 # введено число 3486, то надо вывести число 6843.
 
+# ссылка на блок-схему
+# https://drive.google.com/file/d/1Br7etzOCWARCVSA9do66tzF_1eg_xW-B/view?usp=sharing
+
+number = int(input('Введите натуральное число: '))
+revers = 0
+
+while number:
+    dig = number % 10
+    number = number // 10
+    revers = revers * 10 + dig
+
+print(revers)
