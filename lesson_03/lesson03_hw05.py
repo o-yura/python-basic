8570806fcfdd207deb4d82a863f98245a474b922
yura <ogay_yura@mail.ru>
2018-11-05 11:34:14 +0300

    Выполнено задание 5

--- lesson_03/lesson03_hw05.py
+++ lesson_03/lesson03_hw05.py
@@ -1,2 +1,20 @@
 # 5. В массиве найти максимальный отрицательный элемент.
 # Вывести на экран его значение и позицию в массиве.
+
+data = [5, 2, 3, 5, 0, -4, 13, 334, -65, -4, 8, 45, 34, 4]
+
+max_dig = 0
+trigger = True
+
+for i in data:
+    if i < 0:
+        if trigger:
+            max_dig = i
+            trigger = False
+        if i > max_dig:
+            max_dig = i
+
+if not trigger:
+    print('{} в позиции {}'.format(max_dig, data.index(max_dig)))
+else:
+    print('Отрицательных чисел нет')
