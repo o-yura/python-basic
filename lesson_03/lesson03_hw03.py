28aede02a5096dcf662074077c8b29f945c00b3f
yura <ogay_yura@mail.ru>
2018-11-05 10:44:30 +0300

    Переименованы переменные минимума и максимума

--- lesson_03/lesson03_hw03.py
+++ lesson_03/lesson03_hw03.py
@@ -2,16 +2,16 @@
 # максимальный элементы.
 
 data = [5, 2, 3, 5, 0, 4, 13, 34, 65, 4, 8, 45]
-min = 0
-max = 0
+min_dig = 0
+max_dig = 0
 for i in data:
-    if i < min:
-        min = i
-    elif i > max:
-        max = i
+    if i < min_dig:
+        min_dig = i
+    elif i > max_dig:
+        max_dig = i
 
-index_min = data.index(min)
-index_max = data.index(max)
-data[index_min] = max
-data[index_max] = min
+index_min = data.index(min_dig)
+index_max = data.index(max_dig)
+data[index_min] = max_dig
+data[index_max] = min_dig
 print(data)
