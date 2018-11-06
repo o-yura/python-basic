769a7a7261453f64952e49bf3652f8827f7f578a
yura <ogay_yura@mail.ru>
2018-11-06 12:57:03 +0300

    Исправлены исходные значения max_dig и min_dig

--- lesson_03/lesson03_hw06.py
+++ lesson_03/lesson03_hw06.py
@@ -2,9 +2,10 @@
 # между минимальным и максимальным элементами.
 # Сами минимальный и максимальный элементы в сумму не включать.
 
-data = [5, 2, 3, 555, 0, -4, 13, 334, -65, -4, 8, 45, 34, 4]
-max_dig = 0
-min_dig = 0
+# data = [5, 2, 3, 555, 0, -4, 13, 334, -65, -4, 8, 45, 34, 4]
+data = [47, 73, 29, 17, 25, 67, 8, 48, 95, 85]
+max_dig = data[0]
+min_dig = data[0]
 
 for i in data:
     if i > max_dig:
@@ -14,7 +15,7 @@ for i in data:
 
 first_index = data.index(min_dig)
 second_index = data.index(max_dig)
-if max_dig > min_dig:
+if first_index > second_index:
     first_index = data.index(max_dig)
     second_index = data.index(min_dig)
 
