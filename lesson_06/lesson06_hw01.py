1e046595b8d0f3c90d2da96fe9998f1c7a3fae7b
yura <ogay_yura@mail.ru>
2018-11-15 01:48:24 +0300

    Отформатирован анализ

--- lesson_06/lesson06_hw01.py
+++ lesson_06/lesson06_hw01.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # 1. Подсчитать, сколько было выделено памяти под переменные в ранее
 # разработанных программах в рамках первых трех уроков.
 # Проанализировать результат и определить программы с наиболее
@@ -7,8 +8,8 @@
 #
 # P.S. Напишите в комментариях версию Python и разрядность ОС.
 
-import random
-import sys
+from random import randint
+from sys import getsizeof
 from collections import Iterable
 from collections import Counter
 
@@ -17,11 +18,9 @@ from collections import Counter
 def calc_size(var_list):
     def _get_no_iter(unit):
         unit_type = type(unit)
-        unit_size = sys.getsizeof(unit)
+        unit_size = getsizeof(unit)
 
         spam_sizes.append(unit_size)
-
-        # print(unit, unit_type, unit_size)
 
         if isinstance(unit, Iterable):
             if unit_type is dict:
@@ -37,13 +36,24 @@ def calc_size(var_list):
     return sum(spam_sizes)
 
 
+# Вывод статистической информации по переменным
+def output_info(data):
+    print('Список переменных:')
+    print('\t{0:>3} {1:<12} {2:<10}'.format('NN', 'Название', 'Тип'))
+    for i in data:
+        j = 0
+        for key, value in i.items():
+            j += 1
+            print('\t{0:>3} {1:<12} {2:<10}'.format(j, key, str(type(value))))
+
+
 # Выбрано задание 4 урока 3.
 # Определить, какое число в массиве встречается чаще всего.
 
 # Вариант 1. Перебором, общим методом.
 
 def get_max_1(n):
-    data = [random.randint(0, 10) for _ in range(n)]
+    data = [randint(0, 10) for _ in range(n)]
     spam_data = {}
     for i in data:
         if spam_data.get(i):
@@ -67,7 +77,7 @@ def get_max_1(n):
 # Вариант 2. Используем collections.
 
 def get_max_2(n):
-    data = [random.randint(0, 10) for _ in range(n)]
+    data = [randint(0, 10) for _ in range(n)]
     spam_data = Counter(data)
 
     # Сбор информации по используемым переменным
@@ -76,18 +86,22 @@ def get_max_2(n):
     return spam_data.most_common(1)[0][0]
 
 
+# Временный список для сбора переменных в разных функциях
 var_list = []
 print('Решение задачи вариантом 1:')
 print('Результат:', get_max_1(10))
-print(var_list)
-print('Под переменные выделено:', calc_size(var_list), 'байт памяти')
+output_info(var_list)
+# Вычитаем размер служебного списка (var_list) для сбора информации
+print('Под переменные выделено:', calc_size(var_list) - getsizeof(var_list), 'байт памяти')
 del var_list
+
+print()
 
 var_list = []
 print('Решение задачи вариантом 2:')
 print('Результат:', get_max_2(10))
-print(var_list)
-print('Под переменные выделено:', calc_size(var_list), 'байт памяти')
+output_info(var_list)
+print('Под переменные выделено:', calc_size(var_list) - getsizeof(var_list), 'байт памяти')
 del var_list
 
 ### Python 3.6.6 x86_64
@@ -95,18 +109,31 @@ del var_list
 # Результат выполнения программ:
 #
 # Решение задачи вариантом 1:
-# Результат: 3
-# [{'y': 1, 'max_val': 2, 'max_dig': 3, 'i': 0, 'spam_data': {4: 1, 3: 2, 9: 2, 10: 2, 7: 1, 1: 1, 0: 1}, 'data': [4, 3, 3, 9, 10, 10, 7, 9, 1, 0], 'n': 10}]
-# Под переменные выделено: 2197 байт памяти
+# Результат: 1
+# Список переменных:
+# 	 NN Название     Тип
+# 	  1 y            <class 'int'>
+# 	  2 max_val      <class 'int'>
+# 	  3 max_dig      <class 'int'>
+# 	  4 i            <class 'int'>
+# 	  5 spam_data    <class 'dict'>
+# 	  6 data         <class 'list'>
+# 	  7 n            <class 'int'>
+# Под переменные выделено: 2113 байт памяти
 #
 # Решение задачи вариантом 2:
-# Результат: 10
-# [{'spam_data': Counter({10: 3, 8: 2, 5: 2, 6: 1, 4: 1, 3: 1}), 'data': [8, 8, 10, 10, 5, 10, 6, 5, 4, 3], 'n': 10}]
-# Под переменные выделено: 1549 байт памяти
+# Результат: 2
+# Список переменных:
+# 	 NN Название     Тип
+# 	  1 spam_data    <class 'collections.Counter'>
+# 	  2 data         <class 'list'>
+# 	  3 n            <class 'int'>
+# Под переменные выделено: 1509 байт памяти
 
+#
 # Краткий анализ
 #
-# Во втором варианте решения под переменные выделяется меньше памяти,
+# Во втором варианте решения под переменные выделяется значительно меньше памяти,
 # в связи с их меньшим количеством.
 # В первом варианте используются промежуточные переменные для перебора данных,
 # подсчета и поиска наибольшего значения.
