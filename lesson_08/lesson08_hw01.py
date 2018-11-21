3fb88b8e2fd55b951ac95a90ab1d6a795b370072
yura <ogay_yura@mail.ru>
2018-11-21 16:58:17 +0300

    Выполнено задание 1

--- lesson_08/lesson08_hw01.py
+++ lesson_08/lesson08_hw01.py
@@ -7,15 +7,26 @@
 import hashlib
 from random import randint
 
-N = 100
 
 def gen_string(n):
     string = [chr(randint(97, 122)) for _ in range(n)]
     string = ''.join(string)
     return string
 
-for i in range(N - 1):
-    for j in range(N - )
+
+def hash_calc(string):
+    spam_data = []
+    n = len(string)
+    for i in range(1, n):
+        for j in range(0, n - i + 1):
+            sub_string = string[j:j + i]
+            h_string = hashlib.sha1(sub_string.encode()).hexdigest()
+            if spam_data.count(h_string) == 0:
+                spam_data.append(h_string)
+    return len(spam_data)
 
 
-print(gen_string(N))
+# spam_string = 'papa'
+spam_string = gen_string(100)
+print('В строке: ', spam_string)
+print('Количество различных подстрок: ', hash_calc(spam_string))
