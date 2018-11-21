b5a57cd4ea4323f924a3977706f547c99cdab182
yura <ogay_yura@mail.ru>
2018-11-21 07:47:29 +0300

    Создан генератор строки

--- lesson_08/lesson08_hw01.py
+++ lesson_08/lesson08_hw01.py
@@ -3,3 +3,19 @@
 # Требуется найти количество различных подстрок в этой строке.
 # Для решения задачи рекомендую воспользоваться алгоритмом sha1
 # из модуля hashlib или встроенную функцию hash()
+
+import hashlib
+from random import randint
+
+N = 100
+
+def gen_string(n):
+    string = [chr(randint(97, 122)) for _ in range(n)]
+    string = ''.join(string)
+    return string
+
+for i in range(N - 1):
+    for j in range(N - )
+
+
+print(gen_string(N))
